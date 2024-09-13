e91a2f558cada9d482bff61b897d26266c206318
Althirdy <althirdysanger@gmail.com>
2024-09-13 20:16:27 +0800

    First Commit[Ready for Development]

---
+++ resources/js/Layouts/Main.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+    <div class="max-w-screen-xl mx-auto">
+        <header class="bg-slate-800 text-white">
+            <nav
+                class="p-6 mx-auto max-w-screen-lg flex items-center justify-between"
+            >
+                <Link :href="route('home')">Home</Link>
+            </nav>
+        </header>
+
+        <main class="p-6 mx-auto max-w-screen-lg">
+            <slot />
+        </main>
+    </div>
+</template>
