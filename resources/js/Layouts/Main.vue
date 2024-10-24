9bf2aa2faba5097f6e95eb091cb5789d94b8e0b6
Althirdy <althirdysanger@gmail.com>
2024-10-24 16:15:15 +0800

    Update (tailwind.config.js): adding color variables

--- resources/js/Layouts/Main.vue
+++ resources/js/Layouts/Main.vue
@@ -3,7 +3,7 @@
 
 <template>
     <div class="max-w-screen-xl mx-auto">
-        <header class="bg-slate-800 text-white">
+        <header class="bg-main_blue text-white">
             <nav
                 class="p-6 mx-auto max-w-screen-lg flex items-center justify-between"
             >
