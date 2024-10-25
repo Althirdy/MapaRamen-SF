f960a7d44185a4e358126478e62a83b0cef89021
Althirdy <althirdysanger@gmail.com>
2024-10-25 08:40:37 +0800

    update [login.vue] : Completing the login layout and removing the Main Layout in the app.js , creating a Button Components and changing the font to Roboto. adding a Auth.php routes for Authentication of the user

---
+++ resources/js/Components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+    <button class="rounded-md text-center bg-primary_blue p-2 text-white text-[1.2rem] font-medium disabled:bg-slate-300 disabled:cursor-wait">
+        {{ label }}
+    </button>
+</template>
+<script setup>
+defineProps({
+    label: String,
+    type: {
+        type: String,
+        default: "primary",
+    },
+});
+</script>
