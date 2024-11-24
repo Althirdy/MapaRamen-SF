7b8b3f5bf5be3f63d9b1382187a5da740a843d01
M1cko25 <154508740+M1cko25@users.noreply.github.com>
2024-11-24 20:21:12 +0800

    return ui start

--- resources/js/Pages/Manager/Inventory/Return.vue
+++ resources/js/Pages/Manager/Inventory/Return.vue
@@ -1,3 +1,18 @@
 <script setup>
+import Main from "../../../Layouts/Main.vue";
+import InventoryNav from "../InventoryComponents/InventoryNav.vue";
+import SearchSort from "../InventoryComponents/Search-Sort.vue";
+import InventoryWrapper from "../InventoryComponents/InventoryWrapper.vue";
+import ReturnTable from "../InventoryComponents/ReturnComponents/ReturnTable.vue";
 </script>
-<template></template>
+<template>
+    <Main>
+        <InventoryWrapper>
+            <InventoryNav/>
+            <div>
+                <SearchSort :HasAdd="false" />
+                <ReturnTable/>
+            </div>
+        </InventoryWrapper>
+    </Main>
+</template>
---
+++ resources/js/Pages/Manager/InventoryComponents/ReturnComponents/ReturnTable.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+<template>
+    <div class="overflow-x-auto">
+        <table class="w-full min-w-[1000px] text-left">
+            <tr class="flex p-4 items-center justify-between">
+                <th class="flex-1 w1">Return ID</th>
+                <th class="flex-[2] w2">Ingredients</th>
+                <th class="flex-1 w1">Quantity</th>
+                <th class="flex-1 w1">Measurement</th>
+                <th class="flex-1 w1">Date <br><span>(mm-dd-yyyy)</span></th>
+            </tr>
+        </table>
+    </div>
+</template>
